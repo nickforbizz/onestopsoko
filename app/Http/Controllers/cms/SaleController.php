6d50744f2b5c8f0c12cbaccb468dd26f4b95df86
Nicholas Waruingi <NWaruingi@kemri-wellcome.org>
2025-03-26 09:42:08 +0300

    sales filters

--- app/Http/Controllers/cms/SaleController.php
+++ app/Http/Controllers/cms/SaleController.php
@@ -22,47 +22,86 @@ class SaleController extends Controller
     public function index(Request $request)
     {
         // return datatable of the makes available
-        $data = Sale::orderBy('created_at', 'desc')->get();
+        $data = Sale::orderBy('created_at', 'desc');
+
+        if ($request->input('today_records')==1) {
+            $data->whereDate('created_at', today());
+        }
+
+        $data = $data->get();
+
+        // sales stats
+        $todaySales = Sale::whereDate('created_at', today())->sum('total_amount');
+        $todayQuantity = Sale::whereDate('created_at', today())->sum('quantity');
+        $weeklySales = Sale::whereBetween('created_at', [now()->startOfWeek(), now()->endOfWeek()])->sum('total_amount');
+        $weeklyQuantity = Sale::whereBetween('created_at', [now()->startOfWeek(), now()->endOfWeek()])->sum('quantity');
+        $monthlySales = Sale::whereMonth('created_at', now()->month)->sum('total_amount');
+        $monthlyQuantity = Sale::whereMonth('created_at', now()->month)->sum('quantity');
+        $yearlySales = Sale::whereYear('created_at', now()->year)->sum('total_amount');
+        $yearlyQuantity = Sale::whereYear('created_at', now()->year)->sum('quantity');
+
+        $salesStats = [
+            'today' => [
+            'sales' => $todaySales,
+            'quantity' => $todayQuantity,
+            ],
+            'weekly' => [
+            'sales' => $weeklySales,
+            'quantity' => $weeklyQuantity,
+            ],
+            'monthly' => [
+            'sales' => $monthlySales,
+            'quantity' => $monthlyQuantity,
+            ],
+            'yearly' => [
+            'sales' => $yearlySales,
+            'quantity' => $yearlyQuantity,
+            ],
+        ];
+
+        // Pass the sales stats to the view
+        view()->share('salesStats', $salesStats);
+
         if ($request->ajax()) {
             return Datatables::of($data)
-                ->addIndexColumn()
-                ->editColumn('created_at', function ($row) {
-                    return date_format($row->created_at, 'Y/m/d H:i');
-                })
-                ->editColumn('sales_date', function ($row) {
-                    return date_format($row->sales_date, 'Y/m/d H:i');
-                })
-                ->editColumn('product_id', function ($row) {
-                    return $row->product->title;
-                })
-                ->editColumn('client_id', function ($row) {
-                    return $row->client->name;
-                })
-                ->addColumn('action', function ($row) {
-                    $btn_edit = $btn_del = null;
-                    if (auth()->user()->hasAnyRole('superadmin|admin|editor') || auth()->id() == $row->created_by) {
-                        $btn_edit = '<a data-toggle="tooltip" 
-                                        href="' . route('sales.edit', $row->id) . '" 
-                                        class="btn btn-link btn-primary btn-lg" 
-                                        data-original-title="Edit Record">
-                                    <i class="fa fa-edit"></i>
-                                </a>';
-                    }
+            ->addIndexColumn()
+            ->editColumn('created_at', function ($row) {
+                return date_format($row->created_at, 'Y/m/d H:i');
+            })
+            ->editColumn('sales_date', function ($row) {
+                return date_format($row->sales_date, 'Y/m/d H:i');
+            })
+            ->editColumn('product_id', function ($row) {
+                return $row->product->title;
+            })
+            ->editColumn('client_id', function ($row) {
+                return $row->client->name;
+            })
+            ->addColumn('action', function ($row) {
+                $btn_edit = $btn_del = null;
+                if (auth()->user()->hasAnyRole('superadmin|admin|editor') || auth()->id() == $row->created_by) {
+                $btn_edit = '<a data-toggle="tooltip" 
+                        href="' . route('sales.edit', $row->id) . '" 
+                        class="btn btn-link btn-primary btn-lg" 
+                        data-original-title="Edit Record">
+                        <i class="fa fa-edit"></i>
+                    </a>';
+                }
 
-                    if (auth()->user()->hasRole('superadmin')) {
-                        $btn_del = '<button type="button" 
-                                    data-toggle="tooltip" 
-                                    title="" 
-                                    class="btn btn-link btn-danger" 
-                                    onclick="delRecord(`' . $row->id . '`, `' . route('sales.destroy', $row->id) . '`, `#tb_sales`)"
-                                    data-original-title="Remove">
-                                <i class="fa fa-times"></i>
-                            </button>';
-                    }
-                    return $btn_edit . $btn_del;
-                })
-                ->rawColumns(['action'])
-                ->make(true);
+                if (auth()->user()->hasRole('superadmin')) {
+                $btn_del = '<button type="button" 
+                        data-toggle="tooltip" 
+                        title="" 
+                        class="btn btn-link btn-danger" 
+                        onclick="delRecord(`' . $row->id . '`, `' . route('sales.destroy', $row->id) . '`, `#tb_sales`)"
+                        data-original-title="Remove">
+                    <i class="fa fa-times"></i>
+                    </button>';
+                }
+                return $btn_edit . $btn_del;
+            })
+            ->rawColumns(['action'])
+            ->make(true);
         }
 
         // render view
@@ -84,12 +123,20 @@ class SaleController extends Controller
      */
     public function store(SaleRequest $request)
     {
+        $product = Product::find($request->product_id);
+        if (!$product) {
+            return redirect()->back()->with('error', 'The selected product does not exist');
+        }
+
+        if ($product->quantity < $request->quantity) {
+            return redirect()->back()->with('error', 'Insufficient product quantity');
+        }
+
         // dd($request->validated());
         $sale = Sale::create($request->validated());
 
         if ($sale) {
             // update the product quantity
-            $product = Product::find($request->product_id);
             $product->quantity = $product->quantity - $request->quantity;
             $product->save();
 
@@ -143,6 +190,14 @@ class SaleController extends Controller
     public function update(SaleRequest $request, Sale $sale)
     {
         $sale_old = Sale::find($sale->id);
+        $product = Product::find($request->product_id);
+        if (!$product) {
+            return redirect()->back()->with('error', 'The selected product does not exist');
+        }
+
+        if (($product->quantity + $sale_old->quantity) < $request->quantity) {
+            return redirect()->back()->with('error', 'Insufficient product quantity');
+        }
 
 
         if ($sale->update($request->validated())) {
